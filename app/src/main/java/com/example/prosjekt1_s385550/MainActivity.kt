4e9e3f6a4d97a8a8e0de5f64b5670efd1cff2484
daquynht <daquynh_97@hotmail.com>
2025-09-09 18:16:28 +0200

    Lagt til bakgrunnsbilde og noen knapper.

--- app/src/main/java/com/example/prosjekt1_s385550/MainActivity.kt
+++ app/src/main/java/com/example/prosjekt1_s385550/MainActivity.kt
@@ -1,47 +1,11 @@
 package com.example.prosjekt1_s385550
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.prosjekt1_s385550.ui.theme.Prosjekt1_s385550Theme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Prosjekt1_s385550Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Prosjekt1_s385550Theme {
-        Greeting("Android")
+        setContentView(R.layout.mainlayout)  // <-- peker til XML
     }
 }
