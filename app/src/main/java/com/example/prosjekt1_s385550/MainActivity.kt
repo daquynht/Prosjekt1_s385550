a290967b96bd5e5ca18c63df3604bdf29b4489a5
daquynht <daquynh_97@hotmail.com>
2025-09-11 10:49:27 +0200

    Endret knapper, tekst og lagt til GameActivity

--- app/src/main/java/com/example/prosjekt1_s385550/MainActivity.kt
+++ app/src/main/java/com/example/prosjekt1_s385550/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.prosjekt1_s385550
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainlayout)  // <-- peker til XML
+
+        //Start Spill-knappen
+        val startButton = findViewById<Button>(R.id.button_start)
+
+        //Knappen trykkes og GameActivity åpnes
+        startButton.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
